c0ff111c0ba48b9c331bb206e39358fd4e5acc9a
dracudev <andreujavier99@gmail.com>
2024-11-04 17:32:26 +0100

    LV2 - EX7 (removeFromCart)

--- js/shop.js
+++ js/shop.js
@@ -84,7 +84,7 @@ function buy(id) {
         }         
         else if (product.id === id && isOnCart(id))
             product.quantity++;
-    }        
+    }
     applyPromotionsCart()
     sumCart()
     printCart()
@@ -92,11 +92,17 @@ function buy(id) {
 }
 
 function cleanCart() {
-    cart.length = 0;
+    cart.forEach(product => {
+        product.quantity = 0;
+    });
+    sumCart();
+    printCart();
+    console.log(cart);
 }
 
 function sumCart() {
     cart.total = 0;
+    cart.quantity = 0;
     for (const product of cart) {
         product.subtotal = 0;
         if (product.offer && product.offer.subtotalWithDiscount)
@@ -107,26 +113,38 @@ function sumCart() {
     for (const product of cart) {
         cart.total = cart.total + product.subtotal
     }
+    for (const product of cart) {
+        cart.quantity = cart.quantity + product.quantity
+    }
+    document.getElementById("count_product").textContent = `${cart.quantity}`
 }
 
 function applyPromotionsCart() {
     for (const product of cart) {
         if (product.offer && product.quantity >= product.offer.number)
             product.offer.subtotalWithDiscount = product.price * (1 - product.offer.percent / 100) * product.quantity
+        else if (product.offer && product.quantity < product.offer.number && product.offer.subtotalWithDiscount)
+            product.offer.subtotalWithDiscount = null
     }
 }
 
-function printCart(product) {
+function printCart() {
     const cartList = document.getElementById("cart_list");
     const totalPrice = document.getElementById("total_price")
 
-    for (const product of cart) {
+    for (let index = cart.length - 1; index >= 0; index--) {
+            const product = cart[index];
             let currentRow = Array.from(cartList.rows).find(row => 
                 row.cells[0] && row.cells[0].textContent === product.name);
-            if (currentRow)
+            if (currentRow && product.quantity >= 1)
             {
                 currentRow.cells[2].textContent = `${product.quantity}`;
                 currentRow.cells[3].textContent = `${product.subtotal.toFixed(2)}`;
+            }
+            else if (currentRow && product.quantity === 0)
+            {
+                currentRow.remove();
+                cart.splice(index, 1)
             }
             else
             {
@@ -136,10 +154,22 @@ function printCart(product) {
                 <td>${product.price}</td>
                 <td>${product.quantity}</td>
                 <td>${product.subtotal}</td>
+                <td><button type="button" class="border-0" onClick="removeFromCart(${product.id})"><i class="fa-solid fa-x"></i></button></td>
                 `;
             cartList.appendChild(newRow);
             }
         
     }
     totalPrice.innerHTML = `${cart.total.toFixed(2)}`
+}
+
+function removeFromCart(id) {
+    for (const product of cart) {
+        if (product.id === id)
+            product.quantity--;
+    }
+    applyPromotionsCart();
+    sumCart();
+    printCart();
+    console.log(cart)
 }
